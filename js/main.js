76b5e7272c40e1f560a6a44b44ffa9ff66faf7a7
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-11 15:17:03 +0200

    aggiunta logica js per aggiunta da parte dell'user di prodotti

--- js/main.js
+++ js/main.js
@@ -6,10 +6,30 @@ const listSpesa = ["pane","salame","frutta","biscotti"]
 let listUl = document.querySelector("ul");
 console.log(listUl);
 
-
 // Creazione ciclo while
 let i = 0
 while (i < listSpesa.length) {
     listUl.innerHTML += `<li>${listSpesa[i]}</li>`;
     i++;
 }
+
+// Recupero in pagina items aggiunti dall'utente
+let btnAdd = document.getElementById("add_items")
+btnAdd.addEventListener("click",
+    () => {
+        let singleItem = document.getElementById("items").value;
+        console.log(singleItem);
+        // Aggiunta scelte dall'utente al nostro arrey
+        
+        // listUl.innerHTML += `<li>${singleItem}</li>`;
+        let i = listSpesa.length
+        listSpesa.push(singleItem);
+        while (i < listSpesa.length) {
+            listUl.innerHTML += `<li>${listSpesa[i]}</li>`;
+            i++;
+        }
+    }
+)
+
+
+
