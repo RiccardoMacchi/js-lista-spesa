3e5d8e1b2bd9328d8cfa411b99595647a5c0c2f2
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-12 10:01:22 +0200

    rimosso ciclo per aggiunta prodotti dall'utente

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Creazione lista spesa
-const listSpesa = ["pane","salame","frutta","biscotti"]
+const listSpesa = ["Pane","Salame","Farina","Biscotti"]
 
 // Recupero ul in pagina html
 let listUl = document.querySelector("ul");
@@ -7,6 +7,7 @@ console.log(listUl);
 
 // Creazione ciclo while
 let i = 0
+// Lunghezza della lista -1 che corrisponde alla lunghezza della lista
 while (i < listSpesa.length) {
     listUl.innerHTML += `<li><input id="check_me" type="checkbox">${listSpesa[i]}</li>`;
     i++;
@@ -18,15 +19,10 @@ btnAdd.addEventListener("click",
     () => {
         let singleItem = document.getElementById("items").value;
         console.log(singleItem);
-        // Aggiunta scelte dall'utente al nostro arrey
         
-        // listUl.innerHTML += `<li>${singleItem}</li>`;
-        let i = listSpesa.length
+        // Aggiunta scelte dall'utente al nostro arrey
         listSpesa.push(singleItem);
-        while (i < listSpesa.length) {
-            listUl.innerHTML += `<li><input id="check_me" type="checkbox">${listSpesa[i]}</li>`;
-            i++;
-        }
+        listUl.innerHTML += `<li><input id="check_me" type="checkbox">${listSpesa[listSpesa.length -1]}</li>`;
         // Ripulita del campo input aggiunta items
         document.getElementById("items").value = "";
     }
